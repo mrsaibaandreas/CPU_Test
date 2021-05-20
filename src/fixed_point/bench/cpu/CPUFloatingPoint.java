c26ce20547277bdad401237b01fb32bc457e121d
Bogdan Baciu <bogdanbaciu00@yahoo.com>
2021-05-20 18:21:55 +0300

    Floating point

---
+++ src/fixed_point/bench/cpu/CPUFloatingPoint.java
@@ -0,0 +1,145 @@
+package fixed_point.bench.cpu;
+
+import fixed_point.bench.IBenchmark;
+import fixed_point.logging.ConsoleLogger;
+import fixed_point.logging.ILogger;
+import fixed_point.logging.TimeUnit;
+import fixed_point.timing.ITimer;
+
+import java.util.ArrayList;
+
+public class CPUFloatingPoint implements IBenchmark {
+
+    ArrayList<Character> operators;
+    ITimer timer = new fixed_point.timing.Timer();
+    TimeUnit timeUnit = TimeUnit.Micro;
+    ILogger log = new ConsoleLogger();
+
+    float aF, bF; //single precision
+    float add, sub, mul, div;
+
+    double aD, bD;//double precision
+    double Add, Sub, Mul, Div;
+
+    long endTime;
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void run(Object... params) {
+        operators = new ArrayList<Character>();
+
+        for (int i = 0; i < params.length; i++){
+            operators.add((Character) params[i]);
+        }
+
+        for (Character operator : operators) {
+            timer.start();
+            doOperation(operator, "float");
+            endTime = timer.stop();
+            log.writeTime("Time for operation " + "["+ operator + " & " + "float" +"]"
+                    + " is" , endTime , timeUnit);
+        }
+
+        for (Character operator : operators) {
+            timer.stop();
+            doOperation(operator, "double");
+            endTime = timer.stop();
+            log.writeTime("Time for operation " + "["+ operator + " & " + "double" +"]"
+                    + " is" , endTime , timeUnit);
+        }
+        log.close();
+    }
+
+    private void doOperation(Character operation, String type) {
+
+        switch (type){
+            case "float":
+                switch (operation) {
+                    case '+':
+                        addNumbers('f');
+                    case '-':
+                        subtractNumbers('f');
+                    case '*':
+                        multiplyNumbers('f');
+                    case '/':
+                        divideNumbers('f');
+                }
+            case "double":
+                switch (operation) {
+                    case '+':
+                        addNumbers('d');
+                    case '-':
+                        subtractNumbers('d');
+                    case '*':
+                        multiplyNumbers('d');
+                    case '/':
+                        divideNumbers('d');
+                }
+        }
+
+    }
+
+    private void divideNumbers(char type) {
+        if (type == 'f')
+            div = aF / bF;
+        else
+            Div = aD / bD;
+    }
+
+    private void multiplyNumbers(char type) {
+        if (type == 'f')
+            mul = aF * bF;
+        else
+            Mul = aD * bD;
+    }
+
+    private void subtractNumbers(char type) {
+        if (type == 'f')
+            sub = aF - bF;
+        else
+            Sub = aD - bD;
+    }
+
+    private void addNumbers(char type) {
+        if (type == 'f')
+            add = aF + bF;
+        else
+            Add = aD + bD;
+    }
+
+    @Override
+    public void initialize(Object... params) {
+        this.aF = Float.parseFloat(params[0].toString());
+        this.bF = Float.parseFloat(params[1].toString());
+        this.aD = Double.parseDouble(params[0].toString());
+        this.bD = Double.parseDouble(params[1].toString());
+    }
+
+    @Override
+    public void clean() {
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+
+    @Override
+    public void warmUp() {
+
+        double x = 1234.234, y = 123443.22;
+        aD = x; bD = y;
+        doOperation('+',"double");
+    }
+
+    @Override
+    public String getResult() {
+        return null;
+    }
+
+}
